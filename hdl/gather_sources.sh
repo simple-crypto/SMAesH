aa1e2c14fdf9a9df2bff82fa249fd260f21678a3
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2024-01-31 10:45:28 +0100

    Make error fatal in shell scripts

--- hdl/gather_sources.sh
+++ hdl/gather_sources.sh
@@ -1,26 +1,24 @@
 #! /bin/bash
 
+set -e
+
 # Copy all HDL source files in OUT_DIR.
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-if [ -z "$OUT_DIR" ]
-then
-    echo "OUT_DIR must be set"
-    exit
+if [ -z "$OUT_DIR" ]; then
+	echo "OUT_DIR must be set"
+	exit
 fi
-
 
 mkdir -p $OUT_DIR
 
 # Iterate over each directory
-for var in "$@"
-do
-    dir="$SCRIPT_DIR/$var"
-    files=$(find $dir -name '*.v' -o -name '*.vh')
-    for file in $files
-    do
-        cp $file $OUT_DIR
-        echo "$file copied to $OUT_DIR"
-    done
+for var in "$@"; do
+	dir="$SCRIPT_DIR/$var"
+	files=$(find $dir -name '*.v' -o -name '*.vh')
+	for file in $files; do
+		cp $file $OUT_DIR
+		echo "$file copied to $OUT_DIR"
+	done
 done
